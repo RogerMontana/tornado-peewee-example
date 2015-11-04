2984fbd1bf788c10c94de99cb6fb0f732af40185
Artem K <artem.karpov@yahoo.com>
2015-11-05 00:29:14 +0200

    added page with api-info, redirect to index

--- ocean-server/handlers.py
+++ ocean-server/handlers.py
@@ -1,10 +1,20 @@
 __author__ = 'artem'
 import tornado.web
+import tornado.template as templates
 from moltin.moltin import Moltin
+import sys, inspect
+import os
+
+dirname = os.path.dirname(__file__)
+
+STATIC_PATH = os.path.join(dirname, 'static')
+TEMPLATE_PATH = os.path.join(dirname, 'templates')
+
+current_module = sys.modules[__name__]
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello world")
+        self.render("index.html")
 
 class LoginHandler(tornado.web.RequestHandler):
     def get(self):
@@ -15,7 +25,9 @@ class LoginHandler(tornado.web.RequestHandler):
 
 class InfoApiHandler(tornado.web.RequestHandler):
     def get(self):
-        # m = Moltin()
-        # access_token = m.authenticate()
-        # m.set_access_token(access_token)
-        self.render("index.html")
+        clsmembers = inspect.getmembers(sys.modules[__name__], inspect.isclass)
+        template = templates.Loader(TEMPLATE_PATH)
+        self.write(template.load("api_list.html").generate(info = clsmembers))
+
+
+
