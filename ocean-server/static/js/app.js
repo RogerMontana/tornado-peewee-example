033d2a6addba45183bda836a4dd8ea525e48088f
rozanovz <rozanovz13@gmail.com>
2015-11-23 19:02:38 +0200

    redirect to https

--- ocean-server/static/js/app.js
+++ ocean-server/static/js/app.js
@@ -1,3 +1,5 @@
 (function(){
   $('body').fitText(9);
+  if (window.location.protocol != "https:")
+    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 })();
