4a970a392a3593683ad86987154f5e1487cd4429
rozanovz <rozanovz13@gmail.com>
2015-11-23 19:16:36 +0200

    save

--- ocean-server/static/js/app.js
+++ ocean-server/static/js/app.js
@@ -1,5 +1,3 @@
 (function(){
   $('body').fitText(9);
-  if (window.location.protocol != "https:")
-    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 })();
