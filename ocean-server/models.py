ec771f6e3dd042c855964ca70eb5354502256195
Artem K <artem.karpov@yahoo.com>
2015-11-26 00:33:01 +0200

    Database Models was added

--- ocean-server/models.py
+++ ocean-server/models.py
@@ -1,24 +1,27 @@
+
+import os
 import datetime
 from peewee import *
 
-db = SqliteDatabase('test.db')
+db_proxy = Proxy()
 
-class BaseModel(Model):
-    class Meta:
-        database = db
 
-class Recipe(BaseModel):
+class Recipe(Model):
     title = CharField(unique=True)
     smallTitle = CharField()
-    description = DecimalField()
-    shortDescription = DateTimeField()
+    description = CharField()
+    shortDescription = CharField()
     portion = DecimalField()
     complexity_rate =  DecimalField()
     price = DecimalField()
     photo = CharField()
     ingredientsPhoto = CharField()
     ingredients = CharField()
-    nutrients = CharField()
+    main_ingredient = CharField()
+    nutrients_fat = DecimalField()
+    nutrients_carbohydrates = DecimalField()
+    nutrients_proteins = DecimalField()
+    nutrients_calories = DecimalField()
     publication_date = DateTimeField(default=datetime.datetime.now)
     is_published = BooleanField(default=True)
 
@@ -28,10 +31,11 @@ class Recipe(BaseModel):
             (('title','portion','complexity_rate','price', 'publication_date'), True),
         )
 
-class Ingredient(BaseModel):
-    recipe = ForeignKeyField(Recipe, related_name='recipe_ingredients')
+class Orders(Model):
     type = TextField(unique=True)
     quantity = DecimalField()
+    order_details = TextField()
+    status = TextField()
     created_date = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
@@ -40,14 +44,16 @@ class Ingredient(BaseModel):
             (('type'), True),
         )
 
-class Nutrient(BaseModel):
-    recipe = ForeignKeyField(Recipe, related_name='recipe_nutrients')
-    type = TextField()
-    quantity = DecimalField()
-    created_date = DateTimeField(default=datetime.datetime.now)
+if 'HEROKU' in os.environ:
+    import urlparse, psycopg2
+    urlparse.uses_netloc.append('postgres')
+    url = urlparse.urlparse(os.environ["postgres://zqhwxkznrkqnza:MAKKRjiqJ0j5lziAN78TOCCIjC@ec2-54-247-170-228.eu-west-1.compute.amazonaws.com:5432/d4k58q1gap3hki"])
+    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+    db_proxy.initialize(db)
+else:
+    db = SqliteDatabase('persons.db')
+    db_proxy.initialize(db)
 
-    class Meta:
-        indexes = (
-            # create a unique on Nutrient type
-            (('type'), True),
-        )
+if __name__ == '__main__':
+    db_proxy.connect()
+    db_proxy.create_tables([Recipe, Orders], safe=True)
