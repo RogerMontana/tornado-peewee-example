eabe0f997ce0fa3108f3b46854d1028f4866cde6
Artem K <artem.karpov@yahoo.com>
2016-01-05 22:42:31 +0200

    read order

--- ocean-server/models.py
+++ ocean-server/models.py
@@ -49,7 +49,7 @@ if 'HEROKU' in os.environ:
     import urlparse, psycopg2
     urlparse.uses_netloc.append('postgres')
     url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port, autocommit=True, autorollback=True)
     db_proxy.initialize(db)
 else:
     db = SqliteDatabase('ocean04-dev.db')
@@ -59,13 +59,14 @@ if __name__ == '__main__':
     db_proxy.connect()
     db_proxy.create_table(Recipes , safe=True)
     db_proxy.create_table(Orders , safe=True)
+
     recipe = Recipes.create(
-        title = u'Творожная запеканка',
-        description = u'Диетическая запеканка из творога входит в меню разноплановых диет. Это блюдо используется в спортивном питании, оно полезно людям с различными патологиями, а кроме того, идеально подходит тем, кто стремится похудеть. Приготовленная по нашему рецепту творожная запеканка не перегружает организм калориями и идеально подходит как десерт даже вечером',
+        title = 'Творожная запеканка',
+        description = 'Диетическая запеканка из творога входит в меню разноплановых диет. Это блюдо используется в спортивном питании, оно полезно людям с различными патологиями, а кроме того, идеально подходит тем, кто стремится похудеть. Приготовленная по нашему рецепту творожная запеканка не перегружает организм калориями и идеально подходит как десерт даже вечером',
         price = 100,
-        photo = u'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
-        ingredientsPhoto = u'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
-        ingredients = u'Яйцо куриное|Творог обезжиренный|Крахмал кукурузный|Сахар|Лимон|Ваниль стручковая|Мята|Дрожжи',
+        photo = 'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
+        ingredientsPhoto = 'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
+        ingredients = 'Яйцо куриное|Творог обезжиренный|Крахмал кукурузный|Сахар|Лимон|Ваниль стручковая|Мята|Дрожжи',
         nutrients= '219|17|7|22',
         publication_date = datetime.datetime.now(),
         is_published =True
@@ -75,12 +76,12 @@ if __name__ == '__main__':
     recipe.save()
 
     recipe2 = Recipes.create(
-        title = u'Панна-Котта',
-        description = u'Кажется, все самые вкусные десерты пришли к нам из прекрасной Италии! Нежная североитальянская гостья Панакота или Panna Cotta в переводе "вареный крем" по консистенции напоминает желе и дополняется различными ягодами. У нас сегодня яркая летняя клубника - "ягода любви"!',
+        title = 'Панна-Котта',
+        description = 'Кажется, все самые вкусные десерты пришли к нам из прекрасной Италии! Нежная североитальянская гостья Панакота или Panna Cotta в переводе "вареный крем" по консистенции напоминает желе и дополняется различными ягодами. У нас сегодня яркая летняя клубника - "ягода любви"!',
         price = 100,
-        photo = u'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
-        ingredientsPhoto = u'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
-        ingredients = u'Молоко 3,2% 200гр|Сливки 38%|Ваниль стручковая|Желатин листовой|Сахар|Клубника',
+        photo = 'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
+        ingredientsPhoto = 'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
+        ingredients = 'Молоко 3,2% 200гр|Сливки 38%|Ваниль стручковая|Желатин листовой|Сахар|Клубника',
         nutrients= '510|10|42|24',
         publication_date = datetime.datetime.now(),
         is_published =True
@@ -90,12 +91,12 @@ if __name__ == '__main__':
     recipe2.save()
 
     recipe3 = Recipes.create(
-        title = u'Кролик в томатно-чесночном соусе',
-        description = u'Мясо кролика всегда считалось одним из лучших — оно легкое, нежное, очень вкусное и при этом питательное и полезное. В нем содержится полноценный белок, жир, минеральные вещества и витамины, количество которых значительно выше, чем в мясе свиней, кур и других животных. В этом рецепте мы будем готовить кролик в нежном томатно-чесночном соусе, что добавить немного пикантности данному блюду.',
+        title = 'Кролик в томатно-чесночном соусе',
+        description = 'Мясо кролика всегда считалось одним из лучших — оно легкое, нежное, очень вкусное и при этом питательное и полезное. В нем содержится полноценный белок, жир, минеральные вещества и витамины, количество которых значительно выше, чем в мясе свиней, кур и других животных. В этом рецепте мы будем готовить кролик в нежном томатно-чесночном соусе, что добавить немного пикантности данному блюду.',
         price = 100,
-        photo = u'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
+        photo = 'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
         ingredientsPhoto = 'http://richthediabetic.com/wp-content/uploads/2013/07/Pizza.jpg',
-        ingredients = u'Томаты в собственном соку|Перец болгарский красный|Чеснок|Лук репчатый|Перец чили мини|Лист лавровый|Петрушка|Тимьян|Лимон|Кролик|Кинза|Фольга для запекания|Оливковое масло|Соль',
+        ingredients = 'Томаты в собственном соку|Перец болгарский красный|Чеснок|Лук репчатый|Перец чили мини|Лист лавровый|Петрушка|Тимьян|Лимон|Кролик|Кинза|Фольга для запекания|Оливковое масло|Соль',
         nutrients= '644|67|35|16',
         publication_date = datetime.datetime.now(),
         is_published =True
