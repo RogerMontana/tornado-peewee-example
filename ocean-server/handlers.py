282718c215b3214c6b976fd67953253b1c36c065
Artem K <artem.karpov@yahoo.com>
2016-01-30 16:29:25 +0200

    update backend

--- ocean-server/handlers.py
+++ ocean-server/handlers.py
@@ -104,7 +104,7 @@ class OrdersHandler(PeeweeRequestHandler):
         try:
             order = Orders.create(
             order_details = result["order_details"] + " "+ str(result["total"]),
-            name = result["name"] + " " + result["surname"],
+            name = result["name"],
             timegap = result["timegap"],
             address = result["address"]+"/"+result["appartment"],
             status = "new",
