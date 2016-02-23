0ed452ec93300524a52cd0f437c29c66aceafd33
Artem K <artem.karpov@yahoo.com>
2016-02-23 23:30:23 +0200

    promo EKIPAZH

--- ocean-server/handlers.py
+++ ocean-server/handlers.py
@@ -12,6 +12,7 @@ from models import Recipes
 from models import Orders
 from models import db_proxy
 from peewee import *
+from promo import PromoCodes
 import json
 
 
@@ -147,12 +148,12 @@ class OrdersHandler(PeeweeRequestHandler):
 
 
 class PromoCodeHandler(tornado.web.RequestHandler):
-    response_promotion1 = {'provider':"Test1", 'discount':15}
+    response_promotion1 = {'provider':"Ekipazh", 'discount':25}
     response_promotion2 = {'provider':"Test1", 'discount':10}
     response_error = {'provider':'ERROR', 'discount':0}
-
-    promocodes_p1 = ["test1",]
-    promocodes_p2 = ["test2",]
+    codes = PromoCodes()
+    promocodes_p1 = codes.promo
+    promocodes_p2 = ["test22",]
 
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
@@ -160,12 +161,17 @@ class PromoCodeHandler(tornado.web.RequestHandler):
         self.set_header("Access-Control-Allow-Methods", "POST, OPTIONS")
 
     def post(self, *args, **kwargs):
+        print("promo code used")
         promo = json.loads(self.request.body)
         code = promo["code"]
         if(code in self.promocodes_p1):
             self.write(json.dumps(self.response_promotion1))
+            print(code)
+            self.promocodes_p1.remove(code)
         elif (code in self.promocodes_p2):
             self.write(json.dumps(self.response_promotion2))
+            print(code)
+            self.promocodes_p2.remove(code)
         else:
             self.write(json.dumps(self.response_error))
 
